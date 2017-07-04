eedd1084c8bb17fb370b085620f306dbf07b920a
manofearth <azallay@mail.ru>
2017-07-04 12:54:49 +0900

    Firebase-function to [elastic]search timeline events

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions');
 const indexTimelineEvent = require('./index-timeline-event');
-const queryElasticSearch = require('./query-elastic-search');
+const searchTimelineEvents = require('./search-timeline-events');
 
 exports.indexTimelineEvent = functions.database.ref('/private/{userId}/events/{eventId}').onWrite(indexTimelineEvent);
-exports.queryElasticSearch = functions.https.onRequest(queryElasticSearch);
+exports.queryElasticSearch = functions.https.onRequest(searchTimelineEvents);
---
+++ functions/search-timeline-events.js
@@ -0,0 +1,70 @@
+const request = require('request-promise-native');
+const functions = require('firebase-functions');
+
+function createWildcardsFilter(matchQuery) {
+  return matchQuery
+    .split(' ')
+    .map(token => ({
+      wildcard: {
+        title: '*' + token.toLowerCase() + '*'
+      }
+    }));
+}
+
+function createFilter(matchQuery, ownerId) {
+  return [
+    {
+      term: {
+        ownerId: ownerId
+      }
+    },
+    ...createWildcardsFilter(matchQuery)
+  ];
+}
+
+function createQuery(matchQuery, ownerId) {
+  return {
+    bool: {
+      should: {
+        match: {
+          title: matchQuery
+        }
+      },
+      filter: createFilter(matchQuery, ownerId)
+    }
+  };
+}
+
+function searchTimelineEvents(req, res) {
+
+  //noinspection JSUnresolvedVariable
+  const config = functions.config().elasticsearch;
+
+  const uri = config.uri + '/timelines_ru/event/_search';
+
+  console.log('Requesting', uri, 'for owner', req.query.o, 'with query', req.query.q);
+
+  request({
+    method: 'GET',
+    uri: uri,
+    auth: {
+      username: config.username,
+      password: config.password
+    },
+    json: true,
+    body: {
+      query: createQuery(req.query.q, req.query.o)
+    }
+  }).then(
+    response => {
+      console.log('Success, sending response to client');
+      res.status(200).send(response);
+    },
+    error => {
+      console.error(error);
+      res.status(error.statusCode).send(error.message);
+    }
+  );
+}
+
+module.exports = searchTimelineEvents;
